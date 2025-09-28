d5625ba8473451ece186910dfecc5f922ab4e530
user <hag.hispin12@gmail.com>
2025-09-28 15:13:01 +0300

    Exercise, callback, start,LoginActivity - findviewbyid

--- app/src/main/java/com/example/mathprojectyehonatan/LoginActivity.java
+++ app/src/main/java/com/example/mathprojectyehonatan/LoginActivity.java
@@ -1,5 +1,44 @@
 package com.example.mathprojectyehonatan;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+private EditText userName;
+private Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginactivity);
+        initviews();
+
+
+    }
+
+        private void initviews() {
+            userName = findViewById(R.id.usernn);
+            submit = findViewById(R.id.submitt);
+            submit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.putExtra("userKey", userName.getText().toString());
+                    startActivity(intent);
+                }
+
+            });
+        }
+
+
+    }
 }
